a2d4696e138fb8117ad629f30e6724d351b4b287
Karthik Ram <karthik.ram@gmail.com>
2013-09-23 10:55:27 -0700

    First working example of catching utf8 characters

--- R/test_utf8.R
+++ R/test_utf8.R
@@ -1,8 +1,25 @@
+#' Test for UTF-8 characters
+#'
+#' This test will check every column in a data.frame for possible unicode characters.
+#' @param dat input dataset. Currently only supports \code{data.frame} but will soon support \code{data.table}
+#' @export
+#' @importFrom stringr str_c
+#' @examples \dontrun{
+#' data(iris)
+#' test_utf8(iris)
+#' temp <- structure(list(x = 'The\U3e32393cs quick brown fox jumps over the \U3e32393cs the lazy dog', y = 'Lorem\U3e32393cs ipsum \U3e32393cs dolor', dat = 43.5), .Names = c('x', 'y', 'dat'), row.names = c(NA, -1L), class = 'data.frame')
+#' test_utf8(temp)
+#'}
 test_utf8 <- function(dat) {
-	if(!is(dat, "data.frame"))
-		stop("Can only test data.frames at this time", .call = FALSE)
-
-	 message(sprintf("Now checking %s fields...", ncol(dat)))
-
-	message("done")
-}
+    if (!is(dat, "data.frame")) {
+        stop("Can only test data.frames at this time")
+    }
+    message(sprintf("Now checking %s columns...", ncol(dat)))
+    # Testing for UTF-8 in any columns
+    ut8 <- apply(dat, 2, Encoding)
+    if (any(ut8 == "UTF-8")) {
+        pos <- str_c(as.character(which(ut8 == "UTF-8")), collapse = " ")
+        message(sprintf("UTF-8 characters detected in columns %s", pos))
+    }
+    message("done")
+} 
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# test dat
+# test dat!
+
+This package provides a test suite to ensure that tabular data are correctly formatted. It will ensure that columns do not have unicode characters, numeric columns don't have characters, and that columns of data can be tested to ensure that there are no outliers. This suite would be extremely useful alongside unit tests for code to ensure that data read into R do not have errors in them.
+
+
+## Installation
+
+```coffee
+library(devtools)
+# if you don't have the package, run install.packages("devtools")
+install_github("testdat", "karthikram")
+```
+
+## demos 
+There are several examples in the `demo` folder with error filled datasets. You can set up unit tests for those data and also for standard datasets (see `data()` for datasets available to you) that are part of installed packages.
+
+
+
