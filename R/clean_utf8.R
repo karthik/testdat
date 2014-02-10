a4f8e1a61af3a3fa6130647fd372f5bc3c394c87
Karthik Ram <karthik.ram@gmail.com>
2014-02-09 21:40:46 -0800

    Added first pass of functions dealing with utf-8 issues
    So far:
    
    - One function to detect columns where these characters occur.
    -  A second to clean these out. This could be "applied". However I might have to rewrite this in CPP before production use since it will need to be speedy on large datasets.

---
+++ R/clean_utf8.R
@@ -0,0 +1,20 @@
+#' sanitize_text
+#'
+#' Removes UTF8 characters from text columns
+#' @param input_text The input text
+#' @export
+#' @importFrom assertthat assert_that
+#' @examples \dontrun{
+#' sanitize_text("This is some bad text \U3e32393cs that contains utf-8 characters")
+#'}
+sanitize_text <- function(input_text) {
+  assert_that(is.character(input_text))
+  sanitize.each.element <- function(elem) {
+    ifelse(Encoding(elem)=="unknown", elem,
+      iconv(elem,from=as.character(Encoding(elem)),to="latin1",sub="")
+    )
+  }
+  input_text <- sapply(input_text, sanitize.each.element)
+  names(input_text) <- NULL
+  input_text
+}
