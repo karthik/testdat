3d04165270121b1f6ca4649be76ca90ddd6348b2
Alyssa Frazee <acfrazee@gmail.com>
2014-03-31 16:22:11 -0700

    fixed documentation lines

--- R/test_datecol.R
+++ R/test_datecol.R
@@ -1,8 +1,8 @@
 #' Test for proper formatting in a date column
 #' 
-#' @dat data frame to test
-#' @index index of column containing dates
-#' @form a lubridate function giving the format the dates should be in
+#' @param dat data frame to test
+#' @param index index of column containing dates
+#' @param form a lubridate function giving the format the dates should be in
 #' @value returns FALSE if parsing using the function specified in \code{form} fails. If parsing succeeds, the minimum and maximum years in the date column are printed, so that mis-formatted dates (that still pass parse tests) can be detected.
 test_datecol = function(dat, index, form=ymd){
     require(lubridate)
