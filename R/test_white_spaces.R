5e82aea7a96f33e9d1370f43002a94187507d1ba
anyab <anya.bida@gmail.com>
2014-04-01 14:32:26 -0700

    awesomer first commit

---
+++ R/test_white_spaces.R
@@ -0,0 +1,18 @@
+#' test_for_white_spaces
+#'
+#' Tests white spaces that could end up padding text
+#' @param text Input text. Usually this would be a \code{character} field.
+#' @export
+#' @examples \dontrun{
+#' test_white_spaces(" Karthik Ram")
+#' test_white_spaces(" Karthik Ram ")
+#'}
+test_white_spaces <- function (text)  
+{   wsDetected <- FALSE
+    text1 <- sub("^ +", "", text)
+    if (text1 != text) wsDetected <- TRUE
+    text2 <- sub(" +$", "", text)
+    if (text2 != text) wsDetected <- TRUE
+    if (wsDetected) print("Whitespaces detected.")
+    else print("No whitespaces detected.")
+}
