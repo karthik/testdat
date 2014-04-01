ce71c751f08af0204814d6b37bb1ed7c0198e16f
Hilary <hilary@etsy.com>
2014-04-01 11:38:02 -0700

    added functionality to correct_NA to coerce factors to numeric/character

--- R/correct_NA.R
+++ R/correct_NA.R
@@ -5,7 +5,7 @@
 #' @param dat input dataset. Currently only supports \code{data.frame} but will soon support \code{data.table}
 #' @export
 #' @references 2 Ethan P. White, Elita Baldridge, Zachary T. Brym, Kenneth J. Locey, Daniel J. McGlinn, and 3 and Sarah R. Supp.  1 Nine simple ways to make it easier to (re)use your data.  PeerJ PrePrints. , doi: 10.7287/peerj.preprints.7v2
-correct_NA <- function(dat,NA_alias) {
+correct_NA <- function(dat, factors=FALSE) {
   if (!is(dat, "data.frame")) {
     stop("Can only correct data.frames at this time")
   }
@@ -31,5 +31,29 @@ correct_NA <- function(dat,NA_alias) {
   
   dat[matches] <- NA
   
+  # removing factors, coercing into characters or numeric
+  if(factors=FALSE){
+    
+    # which columns to check
+    check_col <- unique(col(dat)[matches])
+    
+    # how many NAs should there be?
+    NAs_col <- apply(matches,2,sum)[check_col]
+    
+    for(i in 1:length(check_col)){
+      
+      # does coercing to numeric cause extra NAs? if not, then coerce to numeric
+      # if so, coerce to character
+      
+      # no--coerce to numeric
+      if(sum(is.na(as.numeric(as.character(dat[,check_col[i]]))))==NAs_col[i]){
+        dat[,check_col[i]] <- as.numeric(as.character(dat[,check_col[i]]))
+      }
+      else{
+        dat[,check_col[i]] <- as.character(dat[,check_col[i]])
+      }
+    }
+    
+  }
   return(dat)
 } 
